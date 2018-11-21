9be84d7e0d98f7b13ce7a5697dcb7f1142a340e5
MSVermet <mattyvermet@gmail.com>
2018-11-21 16:49:39 +0100

    To root of respository

--- Homework/Week3/convertTXT2JSON.py
+++ Homework/Week3/convertTXT2JSON.py
@@ -3,10 +3,10 @@ import json
 import csv
 
 # open txtfile, add fieldnames, convert to json
-filename = open('Homework/Week3/KNMI_20181101.txt', 'r')
+filename = open('KNMI_20181101.txt', 'r')
 file = csv.DictReader(filename, fieldnames = ("Station", "Date", "Windspeed", "Temperature", "Sunhours", "Precipitation", "Cloudness"))
 out = json.dumps([row for row in file])
 
 # create output file
-with open('Homework/Week3/weather_data.json', 'w') as outfile:
+with open('weather_data.json', 'w') as outfile:
     outfile.write(out)
