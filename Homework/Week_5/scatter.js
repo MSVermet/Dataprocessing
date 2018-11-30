dc2fd75f87a819302ce2837fb9918a4c627c601e
MSVermet <mattyvermet@gmail.com>
2018-11-30 16:45:59 +0100

    Progress

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -1,3 +1,13 @@
-window.onload = function {
+// Matty Vermet, 11320524
+window.onload = function() {
+
   console.log('Yes, you can!')
-}
+  var womenInScience = "http://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/TH_WRXRS.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015"
+  var requests = [d3.json(womenInScience)];
+
+  Promise.all(requests).then(function(response) {
+      console.log(response);
+  }).catch(function(e){
+      throw(e);
+  });
+};
