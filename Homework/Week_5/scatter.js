93f49c057192e77459021591fc5ad9f48cfd12e3
MSVermet <mattyvermet@gmail.com>
2018-12-04 14:09:42 +0100

    Progress

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -6,6 +6,11 @@ window.onload = function() {
   var women_science = "http://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/TH_WRXRS.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015"
   var requests = [d3.json(consumer_confidence), d3.json(women_science)];
   var pdata = [];
+  var pdata07 = [];
+  var pdata09 = [];
+  var pdata11 = [];
+  var pdata13 = [];
+  var pdata15 = [];
 
   Promise.all(requests).then(function(response) {
       mydata = response;
@@ -49,7 +54,26 @@ window.onload = function() {
         ]);
       };
 
-      console.log(pdata);
+      // store data of each year in different list
+      for (var j=0; j<pdata.length; j++){
+        if (pdata[j][1] === 2007){
+          pdata07.push(pdata[j]);
+        }
+        if (pdata[j][1] === 2009){
+          pdata09.push(pdata[j]);
+        }
+        if (pdata[j][1] === 2011){
+          pdata11.push(pdata[j]);
+        }
+        if (pdata[j][1] === 2013){
+          pdata13.push(pdata[j]);
+        }
+        if (pdata[j][1] === 2015){
+          pdata15.push(pdata[j]);
+        }
+      }
+
+      console.log(pdata07);
 
       // set display margins
       var margin = {
@@ -64,22 +88,21 @@ window.onload = function() {
       var w = 600 - margin.right - margin.left;
       var padding = 30;
 
+
       var svg = d3.select("body")
                   .append("svg")
                   .attr("width", 600+padding)
-                  .attr("height", 500+padding);
+                  .attr("height", 500+padding);  
+
 
       var xScale = d3.scaleLinear()
-                     .domain(d3.extent(pdata, function(d) {return d[1]}))
+                     .domain(d3.extent(pdata07, function(d) {return d[3]}))
                      .range([padding, w - padding]);
 
       var yScale = d3.scaleLinear()
-                     .domain([97, d3.max(pdata, function(d) {return d[2];})])
+                     .domain([97, d3.max(pdata07, function(d) {return d[2];})])
                      .range([h+padding, padding]);
 
-      var rScale = d3.scaleLinear()
-                     .domain([0, d3.max(pdata, function(d) {return d[2]; })])
-                     .range([2,5]);
 
       var colorValue = d => d[0];
       var colorLabel = 'Countries';
@@ -89,7 +112,7 @@ window.onload = function() {
       // bl.ocks.org/anonymous/7a65777a1e310b76aca5d499e967c467
 
       var myChart = svg.selectAll("circle")
-         .data(pdata)
+         .data(pdata07)
          .enter()
          .append("circle")
          .attr('transform', 'translate('+margin.left+','+margin.top+')')
@@ -97,36 +120,52 @@ window.onload = function() {
          //.attr("data-legend",function(d) { return d[0]})
          .attr('fill-opacity', 0.6 )
          .attr("cx", function(d){
-           return xScale(d[1]);
+           return xScale(d[3]);
          })
          .attr("cy", function(d){
            return yScale(d[2]);
          })
          .attr("r", 9);
 
+      svg.append("text")
+         .attr("class", "xtext")
+         .attr("x",w/2 +20)
+         .attr("y", h + margin.top + padding + 30)
+         .attr("text-anchor", "middle")
+         .attr("font-size", "13px")
+         .attr("fill", "black")
+         .text("Nr of women in science");
 
-      svg.selectAll("text")
-         .data(pdata)
-         .enter()
-         .append("text")
-         .attr('transform', 'translate('+margin.left+','+margin.top+')')
-         .text(function(d){
-           return d[2];
-         })
-         .attr("x", function(d) {
-           return xScale(d[1]);
-         })
-         .attr("y", function(d) {
-           return yScale(d[2]);
-         })
-         .attr("font-size", "11px")
-         .attr("fill", "black");
+      svg.append("text")
+         .attr("transform", "rotate(-90)")
+         .attr("y", 0-5)
+         .attr("x", -20-(h/2))
+         .attr("dy","1em")
+         .attr("font-size", "13px")
+         .text("Consumer confidence index")
+
+      // svg.selectAll("text")
+      //    .data(pdata07)
+      //    .enter()
+      //    .append("text")
+      //    .attr('transform', 'translate('+margin.left+','+margin.top+')')
+      //    .text(function(d){
+      //      return d[3] + ", " + d[2];
+      //    })
+      //    .attr("x", function(d) {
+      //      return xScale(d[3]);
+      //    })
+      //    .attr("y", function(d) {
+      //      return yScale(d[2]);
+      //    })
+      //    .attr("font-size", "11px")
+      //    .attr("fill", "black");
 
 
 
      // set scale of yaxis
      var vScale = d3.scaleLinear()
-       .domain([97, d3.max(pdata, function(d) {return d[2]; })])
+       .domain([97, d3.max(pdata07, function(d) {return d[2]; })])
        .range([h+padding, 0])
        .nice();
 
@@ -148,14 +187,14 @@ window.onload = function() {
            .style('stroke', '#999');
 
      var hScale = d3.scaleBand()
-       .domain(d3.range(2007, 2016))
+       .domain(d3.extent(pdata07, function(d) { return d[3]}))
        .range([0, w]);
 
        // set xaxis values and scale
        var hAxis = d3.axisBottom()
          .scale(hScale)
          .tickValues(hScale.domain().filter(function(d, i){
-           return !(i % 1);
+           return !(i % 0);
          }));
 
        // apply styles to xaxis
@@ -169,6 +208,70 @@ window.onload = function() {
            hGuide.selectAll('line')
              .style('stroke', '#999');
 
+        d3.selectAll(".m")
+          .on("click", function() {
+            var date = this.getAttribute("value");
+
+            var dataset;
+            if (date == "2007"){
+              dataset = pdata07;
+            }else if (date == "2009"){
+              dataset = pdata09;
+            }else if (date == "2011"){
+              dataset = pdata11;
+            }else if (date == "2013"){
+              dataset = pdata13;
+            }else if (date == "2015"){
+              dataset = pdata15;
+            }
+
+            xScale.domain(d3.extent(dataset, function(d) {return d[3]}))
+            .range([padding, w - padding]);
+            yScale.domain([97, d3.max(dataset, function(d) {return d[2];})])
+            .range([h+padding, padding]);
+
+            hScale.domain(d3.extent(dataset, function(d) { return d[3]}))
+              .range([0, w]);
+
+            hAxis.scale(hScale)
+              .tickValues(hScale.domain().filter(function(d, i){
+                return !(i % 0);
+              }));
+
+            vScale.domain([97, d3.max(dataset, function(d) {return d[2]; })])
+              .range([h+padding, 0])
+              .nice();
+
+            vAxis.scale(vScale)
+              .ticks(10)
+              .tickPadding(5);
+
+            myChart = svg.selectAll("circle")
+                        .data(dataset)
+            myChart.enter()
+            .append("circle")
+            .attr('fill', d => colorScale(colorValue(d)))
+            .attr('fill-opacity', 0.6 )
+            .attr("r", 9);
+
+            myChart.transition()
+                   .duration(700)
+                   .ease(d3.easeLinear)
+                   .attr("cx", function(d){
+                     return xScale(d[3]);
+                   })
+                   .attr("cy", function(d){
+                     return yScale(d[2]);
+                   })
+                   .attr('fill-opacity', 0.6);
+
+            myChart.exit()
+                   .transition()
+                   .duration(700)
+                   .ease(d3.easeCircleIn)
+                   .attr("cx", w)
+                   .remove();
+                 });
 
 
 
