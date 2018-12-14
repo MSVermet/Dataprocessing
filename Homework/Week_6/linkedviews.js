62df1a62ca7520ef9a7af326d7a52955e18c33c0
MSVermet <mattyvermet@gmail.com>
2018-12-14 12:04:29 +0100

    DONE

--- Homework/Week_6/linkedviews.js
+++ Homework/Week_6/linkedviews.js
@@ -285,7 +285,7 @@ window.onload = function() {
 
           // go to new html page
           d3.selectAll(".s").on("click", function () {
-            location.href = "story.html";
+            location.href = "https://msvermet.github.io/Dataprocessing/Homework/Week_6/story.html";
       });
 
     }
